74007e4502c59af592ac0ac08f1efcbe98aa1236
Ariana <104736399+arianasilva14@users.noreply.github.com>
2025-03-26 12:15:54 +0000

    atualizando

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,5 +36,35 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
+    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int linhas[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int tabuleiro[10][10];
+    int navio1;
+
+    printf(" TABULEIRO BATALHA NAVAL \n");
+
+    for(int j = 0; j < 10; j++) {
+        printf("  %c", colunas[j]);
+    }
+
+    printf("\n");
+
+        for(int i = 0; i < 10; i++){
+            printf("%d", linhas[i]);
+
+            for(int j = 0; j < 10; j++){
+
+                printf(" 0 ");
+            }
+
+        }
+
+
+        printf("\n");
+
+
+
+    
+
     return 0;
 }
